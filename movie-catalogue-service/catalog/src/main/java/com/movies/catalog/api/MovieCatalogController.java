770edc290a41897188c45011be69f9e1ba59669f
Seungmin <hsm012362@gmail.com>
2020-08-11 04:50:07 +0900

    eureka discover server, client server implemented

--- movie-catalogue-service/catalog/src/main/java/com/movies/catalog/api/MovieCatalogController.java
+++ movie-catalogue-service/catalog/src/main/java/com/movies/catalog/api/MovieCatalogController.java
@@ -4,6 +4,7 @@ import com.movies.catalog.domain.CatalogItem;
 import com.movies.catalog.domain.Movie;
 import com.movies.catalog.domain.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,13 @@ import java.util.stream.Collectors;
 @RestController
 public class MovieCatalogController {
 
+    private final DiscoveryClient discoveryClient;
     private final RestTemplate restTemplate;
     private final WebClient.Builder webClientBuilder;
+
     @Autowired
-    public MovieCatalogController(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
+    public MovieCatalogController(DiscoveryClient discoveryClient, RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
+        this.discoveryClient = discoveryClient;
         this.restTemplate = restTemplate;
         this.webClientBuilder = webClientBuilder;
     }
@@ -29,10 +33,10 @@ public class MovieCatalogController {
     @GetMapping("{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") final String userId) {
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8081/rating/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject(String.format("http://movie-ratings-server/rating/users/%s" ,userId), UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
-            Movie movie = this.restTemplate.getForObject(String.format("http://localhost:8080/movies/%s", rating.getMovieId()), Movie.class);
+            Movie movie = this.restTemplate.getForObject(String.format("http://movie-info-server/movies/%s", rating.getMovieId()), Movie.class);
             return new CatalogItem(movie.getName(), "Robot trasforms!", rating.getRating());
         }).collect(Collectors.toList());
     }
--- movie-catalogue-service/ratings/src/main/java/com/movies/ratings/domain/UserRating.java
+++ movie-catalogue-service/ratings/src/main/java/com/movies/ratings/domain/UserRating.java
@@ -1,12 +1,21 @@
 package com.movies.ratings.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class UserRating {
+public class UserRating implements Serializable {
     private List<Rating> userRating;
 
     public UserRating() {}
     public UserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
         this.userRating = userRating;
     }
 }
