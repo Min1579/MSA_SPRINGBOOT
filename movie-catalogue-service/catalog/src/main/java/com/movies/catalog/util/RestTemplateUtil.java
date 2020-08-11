718ea086f2abf1f5904325e886703ea4cbac2f54
Seungmin <hsm012362@gmail.com>
2020-08-12 04:44:47 +0900

    add HttpComponentClientHttpRequestFactory for timeout

--- movie-catalogue-service/catalog/src/main/java/com/movies/catalog/util/RestTemplateUtil.java
+++ movie-catalogue-service/catalog/src/main/java/com/movies/catalog/util/RestTemplateUtil.java
@@ -3,6 +3,7 @@ package com.movies.catalog.util;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,7 +12,10 @@ public class RestTemplateUtil {
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
     @Bean
